ad387c3da2d43a3dd026a02265dee6de5d1069ac
Muhammad Mudassar <124991853+Mr-Mudassar@users.noreply.github.com>
2023-03-13 10:10:16 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+let string = "";
+let buttons = document.querySelectorAll("button");
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener("click", (e)=>{
+   if (e.target.innerHTML == "") 
+   {
+    string = eval(string);
+    document.querySelector('input').value = string; 
+   }
+   else if (e.target.innerHTML == "=" & string == "") 
+   {
+    string = "";
+    document.querySelector('input').value = string; 
+   }
+   else if (e.target.innerHTML == "=") 
+   {
+    string = eval(string);
+    document.querySelector('input').value = string; 
+   }
+   else if (e.target.innerHTML == "Delete")
+    {
+   string = string.slice(0, string.length - 1);
+   document.querySelector('input').value = string;
+  }
+   else if(e.target.innerHTML == "Clear") 
+   {
+    string = "";
+    document.querySelector('input').value = string;
+   }
+   else {
+      string = string + e.target.innerHTML;
+      document.querySelector('input').value = string;
+    }
+  })
+})
